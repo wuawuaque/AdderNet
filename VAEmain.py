18a0cc8f332f8d3f7b8d48016516662ef94f1b4a
wuawuaque <52871757+wuawuaque@users.noreply.github.com>
2023-12-08 15:41:02 +0800

    implements of sublayers of CNN by addernet in VQVAE

---
+++ VAEmain.py
@@ -0,0 +1,125 @@
+#Copyright (C) 2019. Huawei Technologies Co., Ltd. All rights reserved.
+
+#This program is free software; you can redistribute it and/or modify it under the terms of the BSD 3-Clause License.
+
+#This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the BSD 3-Clause License for more details.
+
+import os
+from resnet20 import resnet20
+from VAEnet import convVAE
+import torch
+from torch.autograd import Variable
+from torchvision.datasets import CIFAR10
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader 
+import argparse
+import math
+
+parser = argparse.ArgumentParser(description='train-addernet')
+
+# Basic model parameters.
+parser.add_argument('--data', type=str, default='/cache/data/')
+parser.add_argument('--output_dir', type=str, default='/cache/models/')
+args = parser.parse_args()
+
+os.makedirs(args.output_dir, exist_ok=True)  
+
+acc = 0
+acc_best = 0
+epochsum =40
+
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+data_train = CIFAR10(args.data,
+                   transform=transform_train)
+data_test = CIFAR10(args.data,
+                  train=False,
+                  transform=transform_test)
+
+data_train_loader = DataLoader(data_train, batch_size=256, shuffle=True, num_workers=8)
+data_test_loader = DataLoader(data_test, batch_size=100, num_workers=0)
+
+# net = resnet20().cuda()
+net =convVAE().cuda()
+criterion = torch.nn.CrossEntropyLoss().cuda()
+optimizer = torch.optim.SGD(net.parameters(), lr=0.1, momentum=0.9, weight_decay=5e-4)
+
+def adjust_learning_rate(optimizer, epoch):
+    """For resnet, the lr starts from 0.1, and is divided by 10 at 80 and 120 epochs"""
+    # lr = 0.05 * (1+math.cos(float(epoch)/400*math.pi))
+    lr = 0.05 * (1+math.cos(float(epoch)/epochsum*math.pi))
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+        
+def train(epoch):
+    adjust_learning_rate(optimizer, epoch)
+    global cur_batch_win
+    net.train()
+    loss_list, batch_list = [], []
+    for i, (images, labels) in enumerate(data_train_loader):
+        images, labels = Variable(images).cuda(), Variable(labels).cuda()
+ 
+        optimizer.zero_grad()
+ 
+        output = net(images)
+        loss = net.loss_function(images, output[0], output[1], output[2])
+        # loss = criterion(output, labels)
+ 
+        loss_list.append(loss.data.item())
+        batch_list.append(i+1)
+ 
+        if i == 1:
+            print('Train - Epoch %d, Batch: %d, Loss: %f' % (epoch, i, loss.data.item()))
+ 
+        loss.backward()
+        optimizer.step()
+ 
+ 
+def test():
+    global acc, acc_best
+    net.eval()
+    total_mse = 0
+    avg_loss = 0.0
+    with torch.no_grad():
+        for i, (images, labels) in enumerate(data_test_loader):
+            images, labels = Variable(images).cuda(), Variable(labels).cuda()
+            output = net(images)
+            avg_loss += net.loss_function(images, output[0], output[1], output[2]).sum()
+            # pred = output.data.max(1)[1]
+            #归一化mse求和
+            total_mse += torch.sum((output[0] - images) ** 2) / torch.sum(images ** 2)
+
+ 
+    avg_loss /= len(data_test)
+    mse = float(total_mse) / len(data_test)
+    if acc_best < acc:
+        acc_best = acc
+    print('Test Avg. Loss: %f, mse: %f' % (avg_loss.data.item(), mse))
+ 
+ 
+def train_and_test(epoch):
+    train(epoch)
+    test()
+ 
+ 
+def main():
+    # epoch = 400
+    epoch = epochsum
+    for e in range(1, epoch):
+        train_and_test(e)
+    torch.save(net,args.output_dir + 'addernet')
+    print('NET of torch saved')
+ 
+ 
+if __name__ == '__main__':
+    main()
---
+++ VAEnet.py
@@ -0,0 +1,348 @@
+# 2020.01.10-Replaced conv with adder
+#            Huawei Technologies Co., Ltd. <foss@huawei.com>
+from __future__ import print_function
+import abc
+
+import adder
+import torch.nn as nn
+import numpy as np
+import logging
+import torch
+import torch.utils.data
+from torch import nn
+from torch.nn import functional as F
+from nearest_embed import NearestEmbed, NearestEmbedEMA
+
+
+
+def conv3x3(in_planes, out_planes, stride=1):
+    " 3x3 convolution with padding "
+    return adder.adder2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
+
+
+class BasicBlock(nn.Module):
+    expansion=1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(BasicBlock, self).__init__()
+        self.conv1 = conv3x3(inplanes, planes, stride = stride)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+
+class ResNet(nn.Module):
+
+    def __init__(self, block, layers, num_classes=10):
+        super(ResNet, self).__init__()
+        self.inplanes = 16
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.relu = nn.ReLU(inplace=True)
+        self.layer1 = self._make_layer(block, 16, layers[0])
+        self.layer2 = self._make_layer(block, 32, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 64, layers[2], stride=2)
+        self.avgpool = nn.AvgPool2d(8, stride=1)
+        self.fc = nn.Conv2d(64 * block.expansion, num_classes, 1, bias=False)
+        self.bn2 = nn.BatchNorm2d(num_classes)
+        
+        for m in self.modules():
+            if isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+         
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                adder.adder2d(self.inplanes, planes * block.expansion, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion)
+            )
+
+        layers = []
+        layers.append(block(inplanes = self.inplanes, planes = planes, stride = stride, downsample = downsample))
+        self.inplanes = planes * block.expansion
+        for _ in range(1, blocks):
+            layers.append(block(inplanes = self.inplanes, planes = planes))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+
+        x = self.avgpool(x)
+        x = self.fc(x)
+        x = self.bn2(x)
+
+        return x.view(x.size(0), -1)
+
+
+def resnet20(**kwargs):
+    return ResNet(BasicBlock, [3, 3, 3], **kwargs)
+
+
+class AbstractAutoEncoder(nn.Module):
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def encode(self, x):
+        return
+
+    @abc.abstractmethod
+    def decode(self, z):
+        return
+
+    @abc.abstractmethod
+    def forward(self, x):
+        """model return (reconstructed_x, *)"""
+        return
+
+    @abc.abstractmethod
+    def sample(self, size):
+        """sample new images from model"""
+        return
+
+    @abc.abstractmethod
+    def loss_function(self, **kwargs):
+        """accepts (original images, *) where * is the same as returned from forward()"""
+        return
+
+    @abc.abstractmethod
+    def latest_losses(self):
+        """returns the latest losses in a dictionary. Useful for logging."""
+        return
+
+
+
+class ResBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, mid_channels=None, bn=False):
+        super(ResBlock, self).__init__()
+
+        if mid_channels is None:
+            mid_channels = out_channels
+
+        layers = [
+            nn.ReLU(),
+            nn.Conv2d(in_channels, mid_channels,
+                      kernel_size=3, stride=1, padding=1),
+            # adder.adder2d(in_channels, mid_channels,
+            #           kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(mid_channels, out_channels,
+                      kernel_size=1, stride=1, padding=0)
+            # adder.adder2d(mid_channels, out_channels,
+            #           kernel_size=1, stride=1, padding=0)        
+        ]
+        if bn:
+            layers.insert(2, nn.BatchNorm2d(out_channels))
+        self.convs = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return x + self.convs(x)
+
+
+class CVAE(AbstractAutoEncoder):
+    def __init__(self, d, kl_coef=0.1, **kwargs):
+        super(CVAE, self).__init__()
+
+        self.encoder = nn.Sequential(
+            nn.Conv2d(3, d // 2, kernel_size=4,
+                      stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(d // 2),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(d // 2, d, kernel_size=4,
+                      stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(d),
+            nn.ReLU(inplace=True),
+            ResBlock(d, d, bn=True),
+            nn.BatchNorm2d(d),
+            ResBlock(d, d, bn=True),
+        )
+        self.decoder = nn.Sequential(
+            ResBlock(d, d, bn=True),
+            nn.BatchNorm2d(d),
+            ResBlock(d, d, bn=True),
+            nn.BatchNorm2d(d),
+
+            nn.ConvTranspose2d(d, d // 2, kernel_size=4,
+                               stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(d//2),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(d // 2, 3, kernel_size=4,
+                               stride=2, padding=1, bias=False),
+        )
+        self.f = 8
+        self.d = d
+        self.fc11 = nn.Linear(d * self.f ** 2, d * self.f ** 2)
+        self.fc12 = nn.Linear(d * self.f ** 2, d * self.f ** 2)
+        self.kl_coef = kl_coef
+        self.kl_loss = 0
+        self.mse = 0
+
+    def encode(self, x):
+        h1 = self.encoder(x)
+        h1 = h1.view(-1, self.d * self.f ** 2)
+        return self.fc11(h1), self.fc12(h1)
+
+    def reparameterize(self, mu, logvar):
+        if self.training:
+            std = logvar.mul(0.5).exp_()
+            eps = std.new(std.size()).normal_()
+            return eps.mul(std).add_(mu)
+        else:
+            return mu
+
+    def decode(self, z):
+        z = z.view(-1, self.d, self.f, self.f)
+        h3 = self.decoder(z)
+        return torch.tanh(h3)
+
+    def forward(self, x):
+        mu, logvar = self.encode(x)
+        z = self.reparameterize(mu, logvar)
+        return self.decode(z), mu, logvar
+
+    def sample(self, size):
+        sample = torch.randn(size, self.d * self.f ** 2, requires_grad=False)
+        if self.cuda():
+            sample = sample.cuda()
+        return self.decode(sample).cpu()
+
+    def loss_function(self, x, recon_x, mu, logvar):
+        self.mse = F.mse_loss(recon_x, x)
+        batch_size = x.size(0)
+
+        # see Appendix B from VAE paper:
+        # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
+        # https://arxiv.org/abs/1312.6114
+        # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
+        self.kl_loss = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+        # Normalise by same number of elements as in reconstruction
+        self.kl_loss /= batch_size * 3 * 1024
+
+        # return mse
+        return self.mse + self.kl_coef * self.kl_loss
+
+    def latest_losses(self):
+        return {'mse': self.mse, 'kl': self.kl_loss}
+
+
+
+
+class VQ_CVAE(nn.Module):
+    def __init__(self, d, k=8, bn=True, vq_coef=1, commit_coef=0.5, num_channels=3, **kwargs):
+        super(VQ_CVAE, self).__init__()
+
+        self.encoder = nn.Sequential(
+            nn.Conv2d(num_channels, d, kernel_size=4, stride=2, padding=1),
+            # adder.adder2d(num_channels, d, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(d),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(d, d, kernel_size=4, stride=2, padding=1),
+            # adder.adder2d(d, d, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(d),
+            nn.ReLU(inplace=True),
+            ResBlock(d, d, bn=bn),
+            nn.BatchNorm2d(d),
+            ResBlock(d, d, bn=bn),
+            nn.BatchNorm2d(d),
+        )
+        self.decoder = nn.Sequential(
+            ResBlock(d, d),
+            nn.BatchNorm2d(d),
+            ResBlock(d, d),
+            nn.ConvTranspose2d(d, d, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(d),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(
+                d, num_channels, kernel_size=4, stride=2, padding=1),
+        )
+        self.d = d
+        self.emb = NearestEmbed(k, d)
+        self.vq_coef = vq_coef
+        self.commit_coef = commit_coef
+        self.mse = 0
+        self.vq_loss = torch.zeros(1)
+        self.commit_loss = 0
+
+        for l in self.modules():
+            if isinstance(l, nn.Linear) or isinstance(l, nn.Conv2d):
+                l.weight.detach().normal_(0, 0.02)
+                torch.fmod(l.weight, 0.04)
+                nn.init.constant_(l.bias, 0)
+
+        self.encoder[-1].weight.detach().fill_(1 / 40)
+
+        self.emb.weight.detach().normal_(0, 0.02)
+        torch.fmod(self.emb.weight, 0.04)
+
+    def encode(self, x):
+        return self.encoder(x)
+
+    def decode(self, x):
+        return torch.tanh(self.decoder(x))
+
+    def forward(self, x):
+        z_e = self.encode(x)
+        self.f = z_e.shape[-1]
+        z_q, argmin = self.emb(z_e, weight_sg=True)
+        emb, _ = self.emb(z_e.detach())
+        return self.decode(z_q), z_e, emb, argmin
+
+    def sample(self, size):
+        sample = torch.randn(size, self.d, self.f,
+                             self.f, requires_grad=False),
+        if self.cuda():
+            sample = sample.cuda()
+        emb, _ = self.emb(sample)
+        return self.decode(emb.view(size, self.d, self.f, self.f)).cpu()
+
+    def loss_function(self, x, recon_x, z_e, emb):
+        self.mse = F.mse_loss(recon_x, x)
+
+        self.vq_loss = torch.mean(torch.norm((emb - z_e.detach())**2, 2, 1))
+        self.commit_loss = torch.mean(
+            torch.norm((emb.detach() - z_e)**2, 2, 1))
+
+        return self.mse + self.vq_coef*self.vq_loss + self.commit_coef*self.commit_loss
+
+    def latest_losses(self):
+        return {'mse': self.mse, 'vq': self.vq_loss, 'commitment': self.commit_loss}
+
+    def print_atom_hist(self, argmin):
+
+        argmin = argmin.detach().cpu().numpy()
+        unique, counts = np.unique(argmin, return_counts=True)
+        logging.info(counts)
+        logging.info(unique)
+
+
+def convVAE(**kwargs):
+    return VQ_CVAE(16, **kwargs)
